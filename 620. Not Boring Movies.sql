c2c8b5ab787441da67afd6f9b51e6a42ce39b95b
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-04-14 08:26:45 +0530

    Create 620. Not Boring Movies.sql

---
+++ 620. Not Boring Movies.sql
@@ -0,0 +1,21 @@
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| id             | int      |
+| movie          | varchar  |
+| description    | varchar  |
+| rating         | float    |
++----------------+----------+
+id is the primary key (column with unique values) for this table.
+Each row contains information about the name of a movie, its genre, and its rating.
+rating is a 2 decimal places float in the range [0, 10]
+
+Question:
+Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+Return the result table ordered by rating in descending order.
+
+The result format is in the following example.
+# Write your MySQL query statement below
+SELECT * FROM Cinema
+where id%2 !=0 AND description !="boring"
+ORDER BY rating DESC;
